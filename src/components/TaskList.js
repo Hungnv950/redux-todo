1464ac2a267971edc5499f1494b14b1ff610edb1
hungnv950 <hungnv950@gmail.com>
2018-07-19 08:11:31 +0700

    reconfig config.json, delete some package

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux';
-import Modal from 'react-modal';
 import TaskItem from './TaskItem';
 
-Modal.setAppElement('#root');
 class TaskList extends Component {
    constructor() {
     super();
 
-    this.state = {
-      modalIsOpen: false
-    };
-
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
-
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
 
   render() {
     let tasks = this.props.tasks;
@@ -43,27 +22,6 @@ class TaskList extends Component {
             />
           )}
         </ul>
-
-       <div>
-        <button onClick={this.openModal}>Open Modal</button>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          contentLabel="Example Modal"
-        >
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
-        </Modal>
-      </div>
       </div>
     )
   }
